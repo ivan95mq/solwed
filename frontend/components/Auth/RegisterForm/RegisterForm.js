49bbdd0682efa8bf400b0ce197453542176c6049
Iván Moreno <ivan.mq@solwed.es>
2022-11-09 18:10:39 +0100

    Configurando

--- frontend/components/Auth/RegisterForm/RegisterForm.js
+++ frontend/components/Auth/RegisterForm/RegisterForm.js
@@ -1,9 +1,23 @@
+import { Form, Button } from "semantic-ui-react";
+
 export default function RegisterForm(props) {
   const { showLoginForm } = props;
   return (
-    <div>
-      <h1>Formulario de registro</h1>
-      <button onClick={showLoginForm}>Ir al login</button>
-    </div>
+    <>
+      <Form className="login-form">
+        <Form.Input name="name" type="name" placeholder="Nombre" />
+        <Form.Input name="lastname" type="text" placeholder="Apellido" />
+        <Form.Input name="email" type="text" placeholder="Correo Electronico" />
+        <Form.Input name="password" type="text" placeholder="Contraseña" />
+        <div className="actions">
+          <Button type="button" basic>
+            Iniciar sesión
+          </Button>
+          <Button type="submit" className="submit">
+            Registrar
+          </Button>
+        </div>
+      </Form>
+    </>
   );
 }
